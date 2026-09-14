ea3667cd010741829aa5528777a187fa00d4e7bd
Jemmanuel Que <jc.saxophonist0629@gmail.com>
2026-09-15 03:21:22 +0800

    CRUD and login logout with reactjs.

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/names', [NameController::class, 'index']);
     Route::post('/names', [NameController::class, 'store']);
     Route::put('/names/{id}', [NameController::class, 'update']);
     Route::delete('/names/{id}', [NameController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
